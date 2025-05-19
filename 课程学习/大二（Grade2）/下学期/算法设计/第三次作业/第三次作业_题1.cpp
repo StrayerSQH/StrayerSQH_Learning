2d8a283bb8d0ad68be7109fcf7161aebd64f6564
StrayerSQH <154659308+StrayerSQH@users.noreply.github.com>
2025-05-19 20:27:49 +0800

    Add files via upload

---
+++ 课程学习/大二（Grade2）/下学期/算法设计/第三次作业/第三次作业_题1.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    vector<int> height(100000);
+
+    string str;
+    getline(cin, str);
+
+    char t = '[';
+    int i = 1;
+    int j = 0;
+    int num = 0;
+    while (true)
+    {
+        if ('0' <= str[i] && str[i] <= '9')
+        {
+            num = num * 10 + str[i] - '0';
+        }
+        else if (str[i] == ']')
+        {
+            height[j] = num;
+            num = 0;
+            break;
+        }
+        else
+        {
+            height[j] = num;
+            num = 0;
+            j++;
+            i++;
+        }
+
+        i++;
+    }
+
+    int n = height.size();
+    int left = 0;
+    int right = n - 1;
+    int maxSize = 0;
+
+    while (left < right)
+    {
+        int current = min(height[left], height[right]) * (right - left);
+        maxSize = max(current, maxSize);
+
+        if (height[left] < height[right])
+        {
+            left++;
+        }
+        else
+        {
+            right--;
+        }
+    }
+
+    cout << maxSize << endl;
+
+    return 0;
+}
---
+++ 课程学习/大二（Grade2）/下学期/算法设计/第三次作业/第三次作业_题2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	int arr[26];
+	for (int i = 0; i < str.size(); i++)
+	{
+		arr[str[i] - 'a'] = i + 1;
+	}
+	
+	int j = 0;
+	int start = 0, end = 0;
+	int result[500];
+
+	for (int i = 0; i < str.size(); i++)
+	{
+		end = max(end, arr[str[i] - 'a']);
+
+		if (i + 1 == end)
+		{
+			result[j] = end - start;
+			start = i + 1;
+			j++;
+		}
+	}
+
+	cout << "[";
+	int i = 0;
+	for (i = 0; i < j - 1; i++)
+	{
+		cout << result[i] << ",";
+	}
+	cout << result[i];
+	cout << "]";
+
+	return 0;
+}
