00ccb314279c524150d137301d1a562b016889ca
StrayerSQH <154659308+StrayerSQH@users.noreply.github.com>
2025-05-19 20:27:03 +0800

    Add files via upload

---
+++ 课程学习/大二（Grade2）/下学期/算法设计/第二次作业/第二次作业_题1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+bool canJump(vector<int>& nums) {
+    int max_reach = 0;  //存储可以到达的最远位置
+    int n = nums.size(); 
+
+    for (int i = 0; i < n; ++i) {
+        if (i > max_reach) {
+            return false; // 超出数组则不对
+        }
+        //更新max_reach
+        max_reach = max(max_reach, i + nums[i]);
+        if (max_reach >= n - 1) {
+            return true;  //如果到达了最后一个则返回true，表明正确的找到了最后一个
+        }
+    }
+
+    return false;  //默认返回作物
+}
+
+int main() {
+    vector<int> nums1 = { 3, 4, 1, 0, 2, 1 };
+    vector<int> nums2 = { 3, 0, 4, 1, 2, 0, 0, 2 };
+
+    if (canJump(nums1))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+
+    if (canJump(nums2))
+    {
+        cout << "true" << endl;
+    }
+    else
+    {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
---
+++ 课程学习/大二（Grade2）/下学期/算法设计/第二次作业/第二次作业_题2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int MaxProfit(vector<int>& nums)
+{
+	int n = nums.size();
+	int minPrice = nums[0];
+	int maxProfit = 0;
+
+	if (n <= 1)
+	{
+		return 0; //如果输入的值无法满足计算的条件直接返回
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (nums[i] < minPrice)
+		{
+			minPrice = nums[i]; //更新最小值
+		}
+		else 
+		{
+			int currentProfit = nums[i] - minPrice; //计算当前的利润
+			if (currentProfit > maxProfit) 
+			{
+				//如果当前的利润大于之前计算出的最大的利润则重新更新最大的利润
+				maxProfit = currentProfit; 
+			}
+		}
+	}
+
+	return maxProfit; //返回最大值
+}
+
+int main()
+{
+	vector<int> num1 = { 2, 1, 4, 8, 3 };
+	vector<int> num2 = { 3, 4, 5, 1, 2, 6, 6, 7 };
+
+	cout << MaxProfit(num1) << endl;
+	cout << MaxProfit(num2) << endl;
+}
