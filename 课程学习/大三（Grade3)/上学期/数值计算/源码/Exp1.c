948abf0878d9c87a6320e5d90756fb66b7ceb7a3
StrayerSQH <154659308+StrayerSQH@users.noreply.github.com>
2025-10-11 11:16:04 +0800

    Create Exp1.c

---
+++ 课程学习/大三（Grade3)/上学期/数值计算/源码/Exp1.c
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// 第二个方程的导数
+double Function_Two_Derivative(double x)
+{
+	return sin(x) + cos(x);
+}
+
+// 第二个方程
+double Function_Two(double x)
+{
+	return sin(x) - cos(x) + 0.5;
+}
+
+// 第一个方程的导数
+double Function_One_Derivative(double x)
+{
+	return 3 * pow(x, 2) - 2 * x - 1;
+}
+
+// 第一个方程
+double Function_One(double x)
+{
+	return pow(x, 3) - pow(x, 2) - x - 1;
+}
+
+void Question_Two()
+{
+	double down = 0.4, up = 0.5;
+
+	for (int i = 0; i < 100; i++)
+	{
+		double up_tmp = up;
+		up = up - Function_Two(up) / Function_Two_Derivative(up);
+
+		if (abs(up_tmp - up) <= 1e-5)
+		{
+			cout << up << "   " << Function_Two(up) << endl;
+			break;
+		}
+	}
+
+}
+
+void Question_One()
+{
+	double down = 1, up = 2;
+
+	for (int i = 0; i < 100; i++)
+	{
+		double up_tmp = up;
+		up = up - Function_One(up) / Function_One_Derivative(up);
+		
+		if (abs(up_tmp - up) <= 1e-5)
+		{
+			cout << up << "   " << Function_One(up) << endl;
+			break;
+		}
+	}
+}
+
+
+int main()
+{
+	Question_One(); // 计算第一个问题
+	Question_Two(); // 计算第二个问题
+
+	return 0;
+}
